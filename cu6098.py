06454345576d9290bedaa043de40c87a8116b5af
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-10 00:47:24 +0900

    Update cu6098.py

--- cu6098.py
+++ cu6098.py
@@ -1,23 +1,33 @@
 c=[]
-a,b=input().split()
-for i in range(int(a)):
+for i in range(10):
   c.append([])
-  for j in range(int(b)):
-    c[i].append(0)
+  a=input().split()
+  for j in range(10):
+    c[i].append(int(a[j]))
 
-n=int(input())
-for i in range(n): #n막대의 개수
-  l,d,x,y=input().split() #l길이,h방향
-  for j in range(int(l)):
-    if int(d)==0:
-      for i in range(int(l)):
-        c[int(x)-1][int(y)-1+i]=1
-    else:
-      for i in range(int(l)):
-        c[int(x)-1+i][int(y)-1]=1
-  
-for i in range(int(a)):
-  for j in range(int(b)):
+c[1][1]=9
+i=1
+j=1
+while c[i][j+1]==0 or c[i+1][j]==0:
+  if c[i][j]==9: #9인 위치
+    if c[i][j+1]==0: #오른쪽 비어있으면9
+      j+=1
+      c[i][j]=9
+
+    elif c[i][j+1]==1:#오른쪽 막혀있으면
+      if c[i+1][j]==0:#아래로 방향틀기
+        i+=1
+        c[i][j]=9
+
+if c[i][j+1]==2: #오른쪽 2
+            j+=1
+            c[i][j]=9
+if c[i+1][j]==2: #아래쪽 2
+          i+=1
+          c[i][j]=9
+
+for i in range(10):
+  for j in range(10):
     print(c[i][j], end=' ') #공백을 두고 한줄로 출력
   print() #줄바꿈
 
