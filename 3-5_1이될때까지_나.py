628bd5d64e59ae3e496d10d238700a1738759437
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-12 23:11:20 +0900

    Create 3-5_1이될때까지_나.py

---
+++ 3-5_1이될때까지_나.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+count = 0
+while n != 1:
+    if n % m == 0:
+        n = n // m
+        count += 1
+    else:
+        n -= 1
+        count += 1
+print(count)
+'''
+입력예시1
+25 5
+출력예시1: 2
+
+입력예시2
+25 3
+출력예시2: 6
+'''
