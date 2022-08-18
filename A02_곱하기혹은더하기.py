84adef44217002fea2fe96ec13a59f956b917bfd
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-18 22:06:23 +0900

    Create A02_곱하기혹은더하기.py

---
+++ A02_곱하기혹은더하기.py
@@ -0,0 +1,24 @@
+data = input()
+#첫 번째 문자를 숫자로 변경하여 대입
+result = int(data[0])
+for i in range(1, len(data)):
+    #두 수 중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기보다는 더하기 수행
+    num = int(data[i])
+    if num <= 1 or result <= 1:
+        result += num
+    else:
+        result *= num
+print(result)
+'''
+입력:
+02984
+출력: 576
+
+입력:
+567
+출력: 210
+
+입력:
+101034
+출력: 24
+'''
