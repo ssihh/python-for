1b5a300695fa398fa7ce6fa5ece256ea435f35cc
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-18 22:01:42 +0900

    Update A02_곱하기혹은더하기_o.py

--- A02_곱하기혹은더하기_o.py
+++ A02_곱하기혹은더하기_o.py
@@ -1,13 +1,22 @@
 num = str(input())
-k=[]
-result=0
+k = []
+result = 0
 #str(): 문자열 형태로 바꿔줌
 for i in map(int, num):
-  k.append(i)  
+    k.append(i)
 for j in k:
-  if result==0 or result==1:
-    result+=j
-  else:
-    result*=j
- 
+    if result == 0 or result == 1:
+        result += j
+    else:
+        result *= j
+
 print(result)
+'''
+입력:
+02984
+출력: 576
+
+입력:
+567
+출력: 210
+'''
