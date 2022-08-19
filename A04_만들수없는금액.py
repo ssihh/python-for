9b1b93f6ac4436230ca3882dec36701a77abb88f
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-19 23:07:46 +0900

    Update A04_만들수없는금액.py

--- A04_만들수없는금액.py
+++ A04_만들수없는금액.py
@@ -1,16 +1,20 @@
 n = int(input())
 data = list(map(int, input().split()))
 data.sort()
-result=1
-a=0
+result = 1
+
 for i in data:
-  if result<i:
-    break
-  result+=i
+    if i > result:  #i가 result값보다 클 때 탈출
+        break
+    result += i
+
 print(result)
 
 '''
 5
 3 2 1 1 9
-출력예시: 8
+출력: 8
+3
+3 7 8
+출력: 1
 '''
