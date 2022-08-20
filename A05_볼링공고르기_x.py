ab753a05de19b3893821be3b0c446c9d42901086
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-20 23:44:39 +0900

    Update and rename A05_볼링공고르기_.py to A05_볼링공고르기_x.py

--- A05_볼링공고르기_x.py
+++ A05_볼링공고르기_x.py
@@ -1,13 +1,14 @@
+#틀린 풀이 - max 값을 활용안함. 무게가같은 공 3개가 입력되면 틀린 값이 나옴
 n, max = map(int, input().split())
 data = list(map(int, input().split()))
 data.sort()
 
 com = data[0]
-result = int((n * (n - 1)) // 2)
+result = int((n * (n - 1)) // 2) #조합공식 사용
 
 for i in range(1, n):
     if com == data[i]:
-        result -= 1
+        result -= 1 #같은건 카운트 빼주기
     com = data[i]
 
 print(result)
@@ -19,4 +20,7 @@ print(result)
 8 5
 1 5 4 3 2 4 5 2
 출력: 25
+4 3
+2 2 2 3 
+출력: 2
 '''
