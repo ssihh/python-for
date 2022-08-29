b7c690dd75dd5a93e4cf383a821c4ed31e6fa61e
ssihh <49466178+ssihh@users.noreply.github.com>
2022-08-29 23:01:03 +0900

    Update A07_럭키스트레이트.py

--- A07_럭키스트레이트.py
+++ A07_럭키스트레이트.py
@@ -1,20 +1,24 @@
 data = input()
 # print(data[1])
-num =[]
+num = []
 # print(num)
 for i in range(len(data)):
     num.append(int(data[i]))
-  
-h=(int(len(data) / 2))
-result = 0
+
+h = (int(len(data) / 2))
 left, right = 0, 0
 for j in range(h):
     left += num[j]
-for k in range(-1,-h-1,-1):
-  right+=num[k]
+for k in range(-1, -h - 1, -1):
+    right += num[k]
 
-if left==right:
-  print('LUCKY')
+if left == right:
+    print('LUCKY')
 else:
-  print('READY')
-
+    print('READY')
+'''
+123402
+출력1: LUCKY
+7755
+출력2: READY
+'''
