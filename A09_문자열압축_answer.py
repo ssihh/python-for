e00fba61aceb3423ae130be05798f2b037489482
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-03 02:08:38 +0900

    Create A09_문자열압축_answer.py

---
+++ A09_문자열압축_answer.py
@@ -0,0 +1,48 @@
+def solution(s):
+	answer = len(s)
+	for step in range(1, len(s) // 2 + 1):  #1개 단위(step)부터 압축 단위를 늘려가며 확인
+		compressed = ''
+		prev = s[0:step]  #앞에서부터 step 만큼의 문자열 추출
+		count = 1
+	for j in range(step, len(s), step):  #단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+		if prev == s[j:j + step]:  #이전 상태와 동일하다면 압축 횟수(count) 증가
+			count += 1
+		else:  #다른 문자열이 나왔다면(더이상 압축하지 못하는 경우라면)
+			compressed += str(count) + prev if count >= 2 else prev
+			prev = s[j:j + step]  #다시 상태 초기화
+			count = 1
+		compressed += str(count) + prev if count >= 2 else prev  #남아있는 문자열에 대해서 처리
+		answer = min(answer, len(compressed))  #만들어지는 압축 문자열이 가장 짧은 것이 정답
+	return answer
+
+# def solution(s):
+#     answer = len(s)
+#     # 1개 단위(step)부터 압축 단위를 늘려가며 확인
+#     for step in range(1, len(s) // 2 + 1):
+#         compressed = ""
+#         prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
+#         count = 1
+#         # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+#         for j in range(step, len(s), step):
+#             # 이전 상태와 동일하다면 압축 횟수(count) 증가
+#             if prev == s[j:j + step]:
+#                 count += 1
+#             # 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
+#             else:
+#                 compressed += str(count) + prev if count >= 2 else prev
+#                 prev = s[j:j + step] # 다시 상태 초기화
+#                 count = 1
+#         # 남아있는 문자열에 대해서 처리
+#         compressed += str(count) + prev if count >= 2 else prev
+#         # 만들어지는 압축 문자열이 가장 짧은 것이 정답
+#         answer = min(answer, len(compressed))
+#     return answer
+
+s = input()
+print(solution(s))
+'''
+aabbaccc
+7
+ababcdcdababcdcd
+9
+'''
