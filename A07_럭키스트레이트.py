86ce8ab34efd4cd66aa031eba888a274f7db9236
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-02 23:46:35 +0900

    Update A07_럭키스트레이트.py

--- A07_럭키스트레이트.py
+++ A07_럭키스트레이트.py
@@ -4,7 +4,8 @@ num = []
 # print(num)
 for i in range(len(data)):
     num.append(int(data[i]))
-
+# 위처럼 굳이 새로 배열 만들어서 넣어줄 필요없음
+# 바로 int(data[i])로 쓰면됨
 h = (int(len(data) / 2))
 left, right = 0, 0
 for j in range(h):
