7b5d48a9965f246dfc1bdbbbc6c37f0ad6cf7db4
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-03 13:44:53 +0900

    Update A09_문자열압축_answer.py

--- A09_문자열압축_answer.py
+++ A09_문자열압축_answer.py
@@ -1,44 +1,20 @@
-# 그대로 따라 친 코드 왜 안대지
+#들여쓰기에 tap이랑 스페이스랑 섞여들어가면 안됨.
 def solution(s):
-	answer = len(s)
-	for step in range(1, len(s) // 2 + 1):  #1개 단위(step)부터 압축 단위를 늘려가며 확인
-		compressed = ''
-		prev = s[0:step]  #앞에서부터 step 만큼의 문자열 추출
-		count = 1
-	for j in range(step, len(s), step):  #단위(step) 크기만큼 증가시키며 이전 문자열과 비교
-		if prev == s[j:j + step]:  #이전 상태와 동일하다면 압축 횟수(count) 증가
-			count += 1
-		else:  #다른 문자열이 나왔다면(더이상 압축하지 못하는 경우라면)
-			compressed += str(count) + prev if count >= 2 else prev
-			prev = s[j:j + step]  #다시 상태 초기화
-			count = 1
-		compressed += str(count) + prev if count >= 2 else prev  #남아있는 문자열에 대해서 처리
-		answer = min(answer, len(compressed))  #만들어지는 압축 문자열이 가장 짧은 것이 정답
-	return answer
-# 동빈나코드
-# def solution(s):
-#     answer = len(s)
-#     # 1개 단위(step)부터 압축 단위를 늘려가며 확인
-#     for step in range(1, len(s) // 2 + 1):
-#         compressed = ""
-#         prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
-#         count = 1
-#         # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
-#         for j in range(step, len(s), step):
-#             # 이전 상태와 동일하다면 압축 횟수(count) 증가
-#             if prev == s[j:j + step]:
-#                 count += 1
-#             # 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
-#             else:
-#                 compressed += str(count) + prev if count >= 2 else prev
-#                 prev = s[j:j + step] # 다시 상태 초기화
-#                 count = 1
-#         # 남아있는 문자열에 대해서 처리
-#         compressed += str(count) + prev if count >= 2 else prev
-#         # 만들어지는 압축 문자열이 가장 짧은 것이 정답
-#         answer = min(answer, len(compressed))
-#     return answer
-
+  answer = len(s)
+  for step in range(1, len(s) // 2 + 1):  #1개 단위(step)부터 압축 단위를 늘려가며 확인. 1부터 절반까지
+    compressed = '' 
+    prev = s[0:step]  #제일 앞(0)에서부터 step 만큼의 문자열 추출. 문자열을 제일 앞부터 정해진 길이만큼 잘라야 한다고 함.
+    count = 1 #prev, count 상태 초기화
+    for j in range(step, len(s), step):  #단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+      if prev == s[j:j + step]:  #이전 상태와 동일하다면 압축 횟수(count) 증가
+        count += 1
+      else:  #다른 문자열이 나왔다면(더이상 압축하지 못하는 경우라면)
+        compressed += str(count) + prev if count >= 2 else prev # true값 if 조건문 else 값. 1번만 나타난 경우는 prev만 count가 2 이상일땐 2a
+        prev = s[j:j + step]  #다시 prev, count 상태 초기화 prev=b
+        count = 1
+    compressed += str(count) + prev if count >= 2 else prev  #남아있는 문자열에 대해서 처리
+    answer = min(answer, len(compressed))  #만들어지는 압축 문자열이 가장 짧은 것이 정답
+  return answer
 s = input()
 print(solution(s))
 '''
