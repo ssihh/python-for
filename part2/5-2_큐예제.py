1f358e93a3e49ad812ec2b0d7be753e90e6aa2fe
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-09 22:50:37 +0900

    Create 5-2_큐예제.py

---
+++ part2/5-2_큐예제.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+queue=deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
+print(list(queue)) #리스트로 변환
