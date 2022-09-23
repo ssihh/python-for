2649b47d615ed65bbb22e98944e648ddf0b161a0
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-24 01:06:02 +0900

    Create 6-11_성적낮은순학생출력.py

---
+++ 6_sort/6-11_성적낮은순학생출력.py
@@ -0,0 +1,21 @@
+n=int(input())
+# array=[map((str,int),input().split()) for _ in range(n)]
+array=[]
+for i in range(n):
+  a, b=input().split()
+  array.append((a, int(b)))
+
+def setting(data):
+  return(data[1])
+
+array.sort(key=setting)
+
+for i in array:
+  print(i[0], end=' ')
+
+'''
+2
+홍길동 95
+이순신 77
+결과: 이순신 홍길동
+'''
