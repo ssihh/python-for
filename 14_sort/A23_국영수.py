c7b020198c2c70164d8392d56766be93ff73b179
ssihh <49466178+ssihh@users.noreply.github.com>
2022-09-24 11:59:03 +0900

    Create A23_국영수.py

---
+++ 14_sort/A23_국영수.py
@@ -0,0 +1,22 @@
+#못품. 정렬 다중 조건 어케하징
+n=int(input())
+a=[]
+for i in range(n):
+  name, k, m, e=input().split()
+  a.append((name,int(k),int(m),int(e)))
+
+a.sort(reverse=True, key=lambda s:s[1]) #국어점수 감소순
+a.sort(key=lambda s:s[2])
+a.sort(reverse=True, key=lambda s:s[3])
+a.sort(key=lambda s:s[0])
+
+for i in range(n):
+  print(a[i])
+
+
+'''
+5 3
+1 2 5 4 3
+5 5 6 6 5
+결과: 26
+'''
