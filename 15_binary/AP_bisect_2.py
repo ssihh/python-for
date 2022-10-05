4c58963caca2a6d5b01f1f4fed8ee926d2887951
ssihh <49466178+ssihh@users.noreply.github.com>
2022-10-06 02:00:14 +0900

    Update AP_bisect_2.py

--- 15_binary/AP_bisect_2.py
+++ 15_binary/AP_bisect_2.py
@@ -1,5 +1,6 @@
 from bisect import bisect_left, bisect_right
 
+#값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
 def count_by_value(a, left_value, right_value):
   right_index=bisect_right(a,right_value)
   left_index=bisect_left(a,left_value)
