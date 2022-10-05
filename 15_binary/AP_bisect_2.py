58a6f3dc6e77f43e495e909e9db1d4edc2ccdb79
ssihh <49466178+ssihh@users.noreply.github.com>
2022-10-06 01:58:51 +0900

    Create AP_bisect_2.py

---
+++ 15_binary/AP_bisect_2.py
@@ -0,0 +1,11 @@
+from bisect import bisect_left, bisect_right
+
+def count_by_value(a, left_value, right_value):
+  right_index=bisect_right(a,right_value)
+  left_index=bisect_left(a,left_value)
+  return right_index-left_index
+
+a=[1,2,3,3,3,3,4,4,8,9]
+
+print(count_by_value(a,4,4))
+print(count_by_value(a,-1,3))
